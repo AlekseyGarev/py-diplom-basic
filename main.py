668ce7116bd9fcf540dd3166124e7c64051fef27
AlekseyGarev <garev91@gmail.com>
2025-04-22 17:45:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import requests
+import json
+from datetime import datetime
+from tqdm import tqdm
+from tokens import vk_token 
+
+name_list = []
+result_data = []
+
+id_vk = input('Введите айди пользователя:')
+ 
+n_photo = input('Введите кол-во фото для скачивания(По умолчанию 5шт):')
+if n_photo == '':
+    n_photo = 5
+
+ya_token = input('Введите токен с Полигона Яндекс.Диска:')
+
+down_photo_url = 'https://api.vk.com/method'
+params = {
+    'access_token': vk_token,
+    'owner_id': id_vk,
+    'count': n_photo,
+    'album_id': 'profile',
+    'v': '5.199',
+    'extended': 1
+}
+
+response = requests.get(f'{down_photo_url}/photos.get', params=params)
+
+data = response.json()
+total_photos = data['response']['count']
+print(f'Найдено:{total_photos} фото!')
+
+yd_url = 'https://cloud-api.yandex.net/v1/disk/resources'
+params = {'path': 'Photo'}
+headers = {'Authorization': ya_token}
+response = requests.put(yd_url, params=params, headers=headers)
+
+with tqdm(total=n_photo, desc="Процесс выполнения", unit="фото") as pbar:
+
+    for item in data['response']['items']:
+        img_url = item['orig_photo']['url']
+        img_name = item['likes']['count']
+        if img_name in name_list:
+            current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+            img_name = f"{item['likes']['count']}_{current_time}"
+
+        result_data.append({
+            "file_name": f"{img_name}.jpg",
+            "size": item['sizes'][-1]['type']
+        })
+
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        params = {
+        'path': f'Photo/{img_name}',
+        'url': {img_url}
+        }
+
+        response = requests.post(upload_url, params=params, headers=headers)
+    
+        pbar.set_postfix(file=f"{img_name}.jpg")
+        pbar.update(1)
+
+with open('result.json', 'w', encoding='utf-8') as f:
+    json.dump(result_data, f, ensure_ascii=False, indent=4)
